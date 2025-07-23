d8a6549ef673f3f5a4c00f4e962603ad7c2a787c
Stepan Kovar <kovar.stepan@protonmail.com>
2025-07-23 12:36:47 +0200

    feat(svelte): add React preprocessing support to svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocessReact from "svelte-preprocess-react/preprocessReact";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), preprocessReact()],
 
        kit: {
                adapter: adapter({ fallback: 'index.html' })
