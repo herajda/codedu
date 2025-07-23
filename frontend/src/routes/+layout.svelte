c4cf5b2f12c770db31f6bdc6d6745d4eeaf0d591
Štěpán Kovář <kovar.stepan@protonmail.com>
2025-07-23 19:12:16 +0200

    style: allow chat page to use full width

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -215,7 +215,7 @@
       </div>
     </div>
 
-    <main class="container mx-auto flex-1 p-4 flex flex-col">
+    <main class="w-full flex-1 p-4 flex flex-col overflow-hidden">
       <slot />
     </main>
 
