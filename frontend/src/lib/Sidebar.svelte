b2e94b226eddf3aa92d4569e7e2003fbe8262444
Štěpán Kovář <kovar.stepan@protonmail.com>
2025-08-09 18:00:30 +0200

    Fix sidebar assignments link

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -52,7 +52,7 @@
             {#if $auth?.role === 'student'}
               <li><a class={$page.url.pathname===`/classes/${c.id}/overview` ? 'active' : ''} href={`/classes/${c.id}/overview`} on:click={() => sidebarOpen.set(false)}>Overview</a></li>
             {/if}
-            <li><a class={$page.url.pathname===`/classes/${c.id}` ? 'active' : ''} href={`/classes/${c.id}`} on:click={() => sidebarOpen.set(false)}>Assignments</a></li>
+            <li><a class={$page.url.pathname===`/classes/${c.id}/assignments` ? 'active' : ''} href={`/classes/${c.id}/assignments`} on:click={() => sidebarOpen.set(false)}>Assignments</a></li>
             <li><a class={$page.url.pathname===`/classes/${c.id}/files` ? 'active' : ''} href={`/classes/${c.id}/files`} on:click={() => sidebarOpen.set(false)}>Files</a></li>
             <li><a class={$page.url.pathname===`/classes/${c.id}/notes` ? 'active' : ''} href={`/classes/${c.id}/notes`} on:click={() => sidebarOpen.set(false)}>Notes</a></li>
             {#if $auth?.role !== 'student'}
