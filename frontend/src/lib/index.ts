67ff8861635109ac219bb704ece7b0e01981791d
Stepan Kovar <kovar.stepan@protonmail.com>
2025-09-30 14:21:05 +0200

    DeadlinePicker not mentioned in index.ts hotfix

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -5,5 +5,6 @@ export { default as AdminPanel } from './AdminPanel.svelte';
 export { default as NotebookEditor } from './components/NotebookEditor.svelte';
 export { default as Terminal } from './components/Terminal.svelte';
 export { default as RunConsole } from './components/RunConsole.svelte';
+export { default as DeadlinePicker } from './components/DeadlinePicker.svelte'
 export * from './sse';
 export * from './date';
