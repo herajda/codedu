69013f158784ad4e6a6e0747d9d1de8b8ff1c12e
Stepan Kovar <kovar.stepan@protonmail.com>
2026-01-03 16:32:09 +0100

    Introduce dr_scratch CLI for Scratch project analysis, including bad habits detection and mastery assessment.

---
+++ backend/dr_scratch/app/consts_drscratch.py
@@ -0,0 +1,116 @@
+PLUGIN_DEADCODE_LIST_EVENT_VARS = [
+    "event_whenflagclicked",
+    "event_whengreaterthan", "event_whenkeypressed",
+    "event_whenthisspriteclicked", "event_whenbackdropswitchesto",
+    "control_start_as_clone", "procedures_definition",
+    "event_whenbroadcastreceived"
+]
+
+PLUGIN_NON_STARTING_EVENTS = [
+    "event_broadcastandwait", "event_broadcast"
+]
+
+PLUGIN_BACKDROPNAMING_DEFAULT_NAMES = [
+    "backdrop",
+    "fondo",
+    "Fondos",
+    "fons",
+    "atzeko oihala"
+]
+
+PLUGIN_SPRITENAMING_DEFAULT_NAMES = [
+    "Sprite",
+    "Objeto",
+    "Personatge",
+    "Figura",
+    "o actor",
+    "Personaia"
+]
+
+PLUGIN_DEADCODE_LIST_LOOP_BLOCKS = [
+    "control_repeat",
+    "control_forever",
+    "control_if",
+    "control_if_else",
+    "control_repeat_until"
+]
+
+PLUGIN_DEADCODE_LIST_MENU_BLOCKS = [
+    "sensing_touchingobjectmenu",
+    "music_menu_INSTRUMENT",
+    "pen_menu_colorParam",
+    "note",
+    "text2speech_menu_voices",
+    "text2speech_menu_languages",
+    "translate_menu_languages",
+    "music_menu_DRUM",
+    
+]
+
+PLUGIN_MASTERY_MAX_POINTS = 21
+PLUGIN_MASTERY_AVG_POINTS = 3.0
+
+PLUGIN_INIT_ATTRIBUTES = [
+    'costume',
+    'orientation',
+    'position',
+    'size',
+    'visibility'
+]
+
+PLUGIN_INIT_BLOCKMAPPING = {
+    'costume': frozenset([('looks_switchbackdropto', 'absolute'),
+                          ('looks_nextbackdrop', 'relative'),
+                          ('looks_switchcostumeto', 'absolute'),
+                          ('looks_nextcostume', 'relative')]),
+    'orientation': frozenset([('motion_turnright', 'relative'),
+                              ('motion_turnleft', 'relative'),
+                              ('motion_pointindirection', 'absolute'),
+                              ('motion_pointtowards_menu', 'relative')]),
+    'position': frozenset([('motion_movesteps', 'relative'),
+                           ('motion_gotoxy', 'absolute'),
+                           ('motion_goto', 'relative'),
+                           ('motion_glidesecstoxy', 'relative'),
+                           ('motion_glideto', 'relative'),
+                           ('motion_changexby', 'relative'),
+                           ('motion_setx', 'absolute'),
+                           ('motion_changeyby', 'relative'),
+                           ('motion_sety', 'absolute')]),
+    'size': frozenset([('looks_changesizeby', 'relative'),
+                       ('looks_setsizeto', 'absolute')]),
+    'visibility': frozenset([('looks_hide', 'absolute'),
+                             ('looks_show', 'absolute')])
+}
+
+URL_SCRATCH_SERVER = 'https://projects.scratch.mit.edu'
+# URL_GETSB3 = 'http://127.0.0.1:3030/api'
+URL_GETSB3 = 'http://127.0.0.1:3000/api'
+URL_SCRATCH_API = 'https://api.scratch.mit.edu/projects'
+
+
+EXTENDED_RUBRIC = {
+    'EXTENDED_POINTS': 36,
+    'ABSTRACTION': 4,
+    'LOGIC': 4,
+    'SYNCHRONIZATION': 4,
+    'FLOW_CONTROL': 4,
+    'PARALLELISM': 4,
+    'USER_INTERACTIVITY': 4,
+    'DATA_REPRESENTATION': 4,
+    'MATH_OPERATORS': 4,
+    'MOTION_OPERATORS': 4,
+    'BASIC_LIMIT': 0, #  0 < Points <= 7
+    'DEVELOPING_LIMIT': 7, #  7 < Points <= 14
+    'MASTER_LIMIT': 14, #  14 < Points <= 21
+    'ADVANCED_LIMIT': 21, #  21 < Points 
+}
+
+def get_mastery(avg_points: int) -> str:
+    if EXTENDED_RUBRIC['BASIC_LIMIT'] < avg_points <= EXTENDED_RUBRIC['DEVELOPING_LIMIT']:
+        return 'Developing'
+    elif EXTENDED_RUBRIC['DEVELOPING_LIMIT'] < avg_points <= EXTENDED_RUBRIC['MASTER_LIMIT']:
+        return 'Master'
+    elif avg_points <= EXTENDED_RUBRIC['ADVANCED_LIMIT']:
+        return 'Advanced'
+    else:
+        return 'Basic'
---
+++ backend/dr_scratch/app/hairball3/duplicateScripts.py
@@ -0,0 +1,119 @@
+import logging
+from app.hairball3.plugin import Plugin
+from app.hairball3.scriptObject import Script
+
+logger = logging.getLogger(__name__)
+
+
+class DuplicateScripts(Plugin):
+    """
+    Plugin that analyzes duplicate scripts in scratch projects version 3.0
+    """
+
+    def __init__(self, filename, json_project, verbose=False):
+        super().__init__(filename, json_project, verbose=verbose)
+        self.total_duplicate = 0
+        self.sprite_dict = {}
+        self.duplicates = {}
+        self.list_duplicate = []
+        self.list_csv = []
+
+    def get_blocks(self, dict_target):
+        """
+        Gets all the blocks in json format into a dictionary
+        """
+        out = {}
+
+        for dict_key, dicc_value in dict_target.items():
+            if dict_key == "blocks":
+                for blocks, blocks_value in dicc_value.items():
+                    if type(blocks_value) is dict:
+                        out[blocks] = blocks_value
+
+        return out
+
+    def set_sprite_dict(self):
+        """
+        Sets a dictionary containing the scripts of each sprite in Script() format
+        """
+
+        for key, list_dict_targets in self.json_project.items():
+            if key == "targets":
+                for dict_target in list_dict_targets:
+                    sprite_name = dict_target['name']
+                    sprite_blocks = self.get_blocks(dict_target)
+
+                    sprite_scripts = []
+
+                    for key, block in sprite_blocks.items():
+                        if block["topLevel"]:
+                            new_script = Script()
+                            new_script.set_script_dict(block_dict=sprite_blocks, start=key)
+                            sprite_scripts.append(new_script)
+
+                    self.sprite_dict[sprite_name] = sprite_scripts
+
+    def analyze(self):
+        """
+        Searches for intra duplicates of each sprite and outputs them
+        """
+        self.set_sprite_dict()
+
+        for sprite, scripts in self.sprite_dict.items():
+            seen = set()
+            sprite_duplicates = {}
+            for script in scripts:
+                blocks = tuple(script.get_blocks())
+
+                if blocks not in sprite_duplicates.keys():
+                    if len(blocks) > 5:
+                        sprite_duplicates[blocks] = [(script, sprite)]
+                else:
+                    sprite_duplicates[blocks].append((script, sprite))
+
+                seen.add(blocks)
+
+            for key in seen:
+                if key in sprite_duplicates:
+                    if len(sprite_duplicates[key]) <= 1:
+                        sprite_duplicates.pop(key, None)
+
+            self.duplicates.update(sprite_duplicates)
+
+        if self.verbose:
+            logger.debug("duplicate scripts: %s", self.duplicates)
+
+        for key, value in self.duplicates.items():
+            duplicated_scripts = [pair[0] for pair in value]
+            csv_text = [script.get_blocks() for script in duplicated_scripts]
+            script_text = "\n\n".join([script.convert_to_text() for script in duplicated_scripts])
+            self.total_duplicate += sum(1 for _ in duplicated_scripts)
+            self.list_duplicate.append(script_text)
+            self.list_csv.append(csv_text)
+
+        return self.duplicates
+
+    def finalize(self) -> dict:
+
+        self.analyze()
+
+        result = ("%d duplicate scripts found" % self.total_duplicate)
+        result += "\n"
+        for duplicate in self.list_duplicate:
+            result += str(duplicate)
+            result += "\n"
+
+        self.dict_mastery['description'] = result
+        self.dict_mastery['total_duplicate_scripts'] = self.total_duplicate
+        self.dict_mastery['list_duplicate_scripts'] = self.list_duplicate
+        self.dict_mastery['duplicates'] = self.duplicates
+        self.dict_mastery['list_csv'] = self.list_csv
+
+        if self.verbose:
+            logger.info(self.dict_mastery['description'])
+            logger.info(self.dict_mastery['total_duplicate_scripts'])
+            logger.info(self.dict_mastery['list_duplicate_scripts'])
+
+        dict_result = {'plugin': 'duplicate_scripts', 'result': self.dict_mastery}
+
+        return dict_result
